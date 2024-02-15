cd8a2f8eb4d4b168435d645c01cebc33d53823cd
888pocket <dndmdo8@gmail.com>
2024-02-15 23:40:18 +0900

    feat: GET product/list 추가

---
+++ module-product-service/src/main/java/com/joosangah/productservice/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.joosangah.productservice.service;
+
+import com.joosangah.productservice.domain.dto.response.ProductResponse;
+import com.joosangah.productservice.domain.entity.Product;
+import com.joosangah.productservice.mapper.ProductResponseMapper;
+import com.joosangah.productservice.repository.ProductRepository;
+import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    private final ProductResponseMapper productResponseMapper;
+
+    private Product loadProduct(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public List<ProductResponse> loadProducts() {
+        return productResponseMapper.toDtoList(productRepository.findAll());
+    }
+}
