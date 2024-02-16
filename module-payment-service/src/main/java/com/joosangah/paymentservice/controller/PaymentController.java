e80b549818d257e82b6d2772405116b92d50ad2b
888pocket <dndmdo8@gmai.com>
2024-02-17 02:18:19 +0900

    feat: 결제 과정에 재고 관련 로직 추가

--- module-payment-service/src/main/java/com/joosangah/paymentservice/controller/PaymentController.java
+++ module-payment-service/src/main/java/com/joosangah/paymentservice/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.joosangah.paymentservice.common.domain.User;
 import com.joosangah.paymentservice.domain.dto.request.PaymentRequest;
 import com.joosangah.paymentservice.service.PaymentService;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.impl.execchain.RequestAbortedException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,9 +23,10 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/start")
-    public void startPayment(@RequestBody PaymentRequest request) {
+    public void startPayment(@RequestBody PaymentRequest request) throws RequestAbortedException {
         User user = userFeignClient.getUser();
-        // TODO: 재고 체크
+        // 재고, 오픈 일정 체크
+        paymentService.validateProductForPurchase(request.getProductId(), request.getAmount());
 
         paymentService.startPayment(user, request);
     }
